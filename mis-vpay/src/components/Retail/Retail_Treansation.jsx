f70be74da55e014423f6bb3a0fc52eacfa15725c
Uday-glict <84915848+Uday-glict@users.noreply.github.com>
2023-08-24 11:31:57 +0530

    Update Retail_Treansation.jsx
    Retails

--- mis-vpay/src/components/Retail/Retail_Treansation.jsx
+++ mis-vpay/src/components/Retail/Retail_Treansation.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Retail.css";
 import DropDown from "./DropDown";
 import Filter from "./Filter";
@@ -8,19 +8,63 @@ import msg from "../Assets/images/msg_icon.png";
 import calender from "../Assets/images/date-time_icon.png";
 import SideBar from "../Shared/SideBar";
 import Navbar from "../Shared/Navbar";
-import { Link } from "react-router-dom";
 import SalesTable from "../Table/SalesTable";
 import datetime from "../Assets/images/Vector (Stroke).png";
+import ScheduleModal from "../Shared/Modal/ScheduleModal";
 const Retail_Treansation = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [hide, setHide] = useState(false);
+  const [scheme_details, setSchemeDetails] = useState([]);
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
+  const [select_type, setSelectType] = useState("");
+  const [assetClass, setAssetClass] = useState();
+  const [transaction_summary_report, setTransactionSummaryReport] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "http://127.0.0.1:3000/api/v1/scheme_details"
+        );
+        const data = await response.json();
+        setSchemeDetails(data);
+      } catch (error) {
+        console.error("error fetching scheme details", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const fetchTransactionSummary = async () => {
+    try {
+      const formattedStartDate = startDate.split("-").reverse().join("/");
+      const formattedEndDate = endDate.split("-").reverse().join("/");
+
+      const queryParams = new URLSearchParams({
+        start_date: formattedStartDate,
+        end_date: formattedEndDate,
+        asset_class: 1,
+        select_type: select_type,
+        employee_code: 2941,
+      });
+      const response = await fetch(
+        `http://127.0.0.1:3000/api/v1/summary_transactions?${queryParams}`
+      );
+      const data = await response.json();
+      setTransactionSummaryReport(data);
+    } catch (error) {
+      console.error("error fetching transaction summary data", error);
+    }
+  };
 
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
   };
 
   const togglehide = () => {
-    setHide(!hide);
+    fetchTransactionSummary();
+    setHide(true);
   };
 
   return (
@@ -32,7 +76,7 @@ const Retail_Treansation = () => {
           className={` ${sidebarOpen ? "dashboard-closed" : "dashboard-full"}`}
         >
           <div className="bg-white card m-4">
-            <div className="col-lg-12  ">
+            <div className="col-l-12  ">
               <div className=" rounded-lg p-3">
                 <button
                   className="border-0 w-100 text-left bg-transparent "
@@ -42,23 +86,29 @@ const Retail_Treansation = () => {
                     <b>RETAIL TRANSACTION SUMMARY REPORT ZONE WISE</b>
                   </h5>
                 </button>
-                <div className="card-body">
-                  <div className="row">
-                    <div className="form-group col-md-3 ">
-                      <div className="col-md-6">
-                        <label className="form-lables gap-2">
-                          <b> Start Date </b>
-                        </label>{""}
-                        <img src={datetime} alt="datetime" />
-                      </div>
+                <div>
+                  <div className="col-lg-12 d-flex justify-content-lg-between mt-5 ">
+                    <div className="form-group col-md-3">
+                      <label
+                        className="form-lables"
+                        style={{ marginRight: "5px" }}
+                      >
+                        <b> Start Date </b>
+                      </label>
+                      <img src={datetime} alt="datetime" />
                       <input
                         type="date"
                         className="form-control mt-2"
-                        placeholder="Project Start Date"
+                        placeholder="Project End Date"
+                        value={startDate}
+                        onChange={(e) => setStartDate(e.target.value)}
                       />
                     </div>
                     <div className="form-group col-md-3">
-                      <label className="form-lables">
+                      <label
+                        className="form-lables"
+                        style={{ marginRight: "5px" }}
+                      >
                         <b> End Date </b>
                       </label>
                       <img src={datetime} alt="datetime" />
@@ -66,9 +116,11 @@ const Retail_Treansation = () => {
                         type="date"
                         className="form-control mt-2"
                         placeholder="Project End Date"
+                        value={endDate}
+                        onChange={(e) => setEndDate(e.target.value)}
                       />
                     </div>
-                    <div className="form-group col-md-2">
+                    {/* <div className="form-group col-md-2">
                       <label className="form-lables">
                         <b>Asset Class</b>
                       </label>
@@ -76,6 +128,8 @@ const Retail_Treansation = () => {
                         name=""
                         id="ab"
                         className="form-select form-control mt-2"
+                        value={assetClass}
+                        onChange={(e) => setAssetClass(e.target.value)}
                       >
                         <option value="">All </option>
                         <option value="">Arbitrage </option>
@@ -83,7 +137,7 @@ const Retail_Treansation = () => {
                         <option value="">Equity </option>
                         <option value="">Fixed Income</option>
                       </select>
-                    </div>
+                    </div> */}
                     <div className="form-group col-md-2">
                       <label className="form-lables">
                         <b> Select Type</b>
@@ -92,14 +146,16 @@ const Retail_Treansation = () => {
                         name=""
                         id="ab"
                         className="form-select form-control mt-2"
+                        value={select_type}
+                        onChange={(e) => setSelectType(e.target.value)}
                       >
-                        <option value="">Net sales </option>
-                        <option value="">SIP Gross sales </option>
+                        <option value="netsales">NET SALES </option>
+                        <option value="grosssales">GROSS SALES </option>
                       </select>
                     </div>
-                    <Filter />
-                    <DropDown />
-                    <div className="form-group col-md-2 mt-3">
+                    {/* <Filter />
+                    <DropDown /> */}
+                    {/* <div className="form-group col-md-2 mt-3">
                       <label className="form-lables">
                         <b> Scheme</b>
                       </label>
@@ -108,20 +164,30 @@ const Retail_Treansation = () => {
                         id="ab"
                         className="form-select form-control mt-2"
                       >
-                        <option value="">grid 9 </option>
+                        {scheme_details.map((scheme) => (
+                          <option value="" key={scheme.SCHEME}>
+                            {scheme.SCHEME}
+                          </option>
+                        ))}
                       </select>
-                    </div>
-                    <div className="col-md-5" />
-                    <div className="col-md-2 mt-5">
+                    </div> */}
+                    <div />
+                    <div className="col-md-2 " style={{ marginTop: "30px" }}>
                       <p className="rcorners">
                         <img src={excel} alt="excelicon" /> |{" "}
                         <img src={pdf} alt="pdficon" />|{" "}
                         <img src={msg} alt="msgicon" /> |{" "}
-                        <img id="myImg" src={calender} alt="calendericon" />
+                        <img
+                          id="myImg"
+                          src={calender}
+                          alt="calendericon"
+                          data-bs-toggle="modal"
+                          data-bs-target="#scheduleModal"
+                        />
                       </p>
                     </div>
 
-                    <div className="col-md-1 mt-5">
+                    <div className="col-md-1 " style={{ marginTop: "31px" }}>
                       <button
                         className="btn  BgcolorOrange "
                         onClick={togglehide}
@@ -134,7 +200,16 @@ const Retail_Treansation = () => {
               </div>
             </div>
 
-            <div className="Table">{hide ? <SalesTable /> : ""}</div>
+            <ScheduleModal />
+            <div className="Table">
+              {hide ? (
+                <SalesTable
+                  transaction_summary_report={transaction_summary_report}
+                />
+              ) : (
+                ""
+              )}
+            </div>
           </div>
         </div>
       </div>
