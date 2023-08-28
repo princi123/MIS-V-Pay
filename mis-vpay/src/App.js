661943a4a1e414896dbe88c3ea732fda2b393ec2
PankajTete111 <pankajt0201@gmail.com>
2023-08-28 11:03:02 +0530

    Retail-Aum Pages Done

--- mis-vpay/src/App.js
+++ mis-vpay/src/App.js
@@ -4,6 +4,9 @@ import Home from './components/Dashboard/Home';
 import { BrowserRouter, Routes,Route } from 'react-router-dom';
 import Retail_Transaction from "../src/components/Retail/Retail_Transaction"
 import Table from "../src/components/Table/SalesTable"
+import Search from "./components/Retail/AUM/Search"
+import RetailZhReport from './components/Retail/AUM/RetailZhReport';
+import AumReport from './components/Retail/AUM/AumReport';
 function App() {
   return (
     <div className="App">
@@ -13,6 +16,10 @@ function App() {
         <Route path='/Home' element={<Home/>}/>
         <Route path='/Transaction' element={<Retail_Transaction/>}/>
         <Route path='/Table' element={<Table/>}/>
+        <Route path='/Aum' element={<Search/>}/>
+        <Route path='/RetailZoneReport' element={<RetailZhReport/>}/>
+        <Route path='/Aumreport' element={<AumReport/>}/>
+
       </Routes>
     </BrowserRouter>  
       
