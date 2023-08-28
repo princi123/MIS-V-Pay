882c01a1e527c69e7dcc457f6c3432297e27d758
hemant0672 <hemantkamble339@gmail.com>
2023-08-28 14:47:33 +0530

    Aum React page

--- mis-vpay/src/components/Retail/AUM/AumReport.jsx
+++ mis-vpay/src/components/Retail/AUM/AumReport.jsx
@@ -1,14 +1,14 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import "./AumReport.css"
 import Navbar from "../../Shared/Navbar";
 import SideBar from "../../Shared/SideBar"
 
-const  AumReport=()=> {
+const AumReport = () => {
     const [sidebarOpen, setSidebarOpen] = useState(false);
 
     const toggleSidebar = () => {
         setSidebarOpen(!sidebarOpen);
-      };
+    };
     const data = [
         {
 
@@ -239,60 +239,86 @@ const  AumReport=()=> {
                         className={` ${sidebarOpen ? "dashboard-closed" : "dashboard-full"
                             }`}
                     >
-                        <div className="card mt-4 m-3">
-                            <div className='headline mt-2'>
+                        <div className="card mt-4 m-3" style={{ borderRadius: "10px" }}>
+                            <div className='headline pt-4'>
                                 <h4><b>Retail UFCWise AUM Report</b></h4>
                             </div>
-                            <div className='col-md-12 col-md-12 d-flex justify-content-center mt-3'>
-                                
-                                    <div className='d-flex justify-content-center'>
-                                        <button className='btn export'>Export</button>
-                                    </div>
-                                
+                            <div className='col-md-12 d-flex mt-3 justify-content-end p-2'>
+
+                                <div className="d-flex">
+                                    <button className='btn btn-info'>Export</button>
+                                </div>
                             </div>
-                            <p className='text-center mt-2'><b>Rs. in Lakhs</b></p>
-                            <div className="col-md-12">
-                            <table className="table table-bordered table-striped p-1">
-                                <thead className="Bgcolor">
-                                    <tr className='mid '>
-                                        <th rowSpan="2">Zone</th>
-                                        <th rowSpan="2">Region</th>
-                                        <th rowSpan="2">UFC Code</th>
-                                        <th rowSpan="2">UFC</th>
-                                        <th rowSpan="2">RM Code</th>
-                                        <th rowSpan="2">RM Name</th>
-                                        <th rowSpan="2">Total AUM</th>
-                                        <th colSpan="6">AUM</th>
-                                    </tr>
-                                    <tr>
-                                        <th>Equity</th>
-                                        <th>Hybrid</th>
-                                        <th>Arbitrage</th>
-                                        <th>Passive(ex-Debt)</th>
-                                        <th>Fixed Income</th>
-                                        <th>Cash</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {data.map((item) => (
-                                        <tr key={item.SrNo}>
-                                            <td>{item.Zone}</td>
-                                            <td>{item.Region}</td>
-                                            <td>{item.UFCCode}</td>
-                                            <td>{item.UFC}</td>
-                                            <td>{item.RMCode}</td>
-                                            <td>{item.RMName}</td>
-                                            <td>{item.TotalAUM}</td>
-                                            <td>{item.AUM.Equity}</td>
-                                            <td>{item.AUM.Hybrid}</td>
-                                            <td>{item.AUM.Arbitrage}</td>
-                                            <td>{item.AUM.Passive}</td>
-                                            <td>{item.AUM['Fixed Income']}</td>
-                                            <td>{item.AUM.Cash}</td>
+                    
+                            <div className="container-fluid ">
+                                <div className=" mt-3" style={{ paddingLeft: "10px" }}>
+                                    <div className=" d-flex" style={{ paddingLeft: "10px", paddingBottom: "10px" }}>
+                                        <div className="col-md-3 d-flex">
+                                            <h4>
+                                                <b>SALES</b>
+                                            </h4>
+                                            <h5>
+                                                <b className="gray-color">(In Lakhs)</b>
+                                            </h5>
+                                        </div>
+                                        <div className="col-md-2 list-group">
+                                            <p className="theader">
+                                                <b>All India Region Wise</b>
+                                            </p>
+                                        </div>
+                                        <div className="col-md-2">
+                                            <p className="theader">
+                                                <b>All India UFC Wise </b>
+                                            </p>
+                                        </div>
+                                        <div className="col-md-2">
+                                            <p className="theader">
+                                                <b>All India RM Wise </b>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <table className="table table-bordered p-1">
+                                    <thead className="Bgcolor">
+                                        <tr className='mid '>
+                                            <th rowSpan="2">Zone</th>
+                                            <th rowSpan="2">Region</th>
+                                            <th rowSpan="2">UFC Code</th>
+                                            <th rowSpan="2">UFC</th>
+                                            <th rowSpan="2">RM Code</th>
+                                            <th rowSpan="2">RM Name</th>
+                                            <th rowSpan="2">Total AUM</th>
+                                            <th colSpan="6">AUM</th>
                                         </tr>
-                                    ))}
-                                </tbody>
-                            </table>
+                                        <tr>
+                                            <th className="forright">Equity</th>
+                                            <th className="forright">Hybrid</th>
+                                            <th className="forright">Arbitrage</th>
+                                            <th className="forright">Passive(ex-Debt)</th>
+                                            <th className="forright">Fixed Income</th>
+                                            <th className="forright">Cash</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {data.map((item) => (
+                                            <tr key={item.SrNo}>
+                                                <td className="forleft">{item.Zone}</td>
+                                                <td className="forleft">{item.Region}</td>
+                                                <td className="forleft">{item.UFCCode}</td>
+                                                <td className="forleft">{item.UFC}</td>
+                                                <td className="forleft">{item.RMCode}</td>
+                                                <td className="forleft">{item.RMName}</td>
+                                                <td className="forright">{item.TotalAUM}</td>
+                                                <td className="forright">{item.AUM.Equity}</td>
+                                                <td className="forright">{item.AUM.Hybrid}</td>
+                                                <td className="forright">{item.AUM.Arbitrage}</td>
+                                                <td className="forright">{item.AUM.Passive}</td>
+                                                <td className="forright">{item.AUM['Fixed Income']}</td>
+                                                <td className="forright">{item.AUM.Cash}</td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
                     </div>
@@ -303,3 +329,4 @@ const  AumReport=()=> {
 }
 
 export default AumReport
+
