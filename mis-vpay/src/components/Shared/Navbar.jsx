f526d005122f0e31fa4ac95eedf675a1e503689a
hemant0672 <hemantkamble339@gmail.com>
2023-09-01 17:04:36 +0530

    indentation

--- mis-vpay/src/components/Shared/Navbar.jsx
+++ mis-vpay/src/components/Shared/Navbar.jsx
@@ -9,87 +9,70 @@ import logout from "../Assets/images/logout icon.png";
 import { useNavigate } from "react-router-dom";
 import { removeEmpIdCookie, removeAuthTokenCookie } from "../Auth/Cookie";
 
-
-
 const Navbar = ({ onToggle }) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleLogout = () => {
-      removeEmpIdCookie();
-      removeAuthTokenCookie();
-      navigate("/");
-    };
-  
+  const handleLogout = () => {
+    removeEmpIdCookie();
+    removeAuthTokenCookie();
+    navigate("/");
+  };
 
-    return (
-        <>
-            <nav className="navbar navbar-expand-lg navbar-light bg-white p-0" >
-                <div className="container-fluid " id="nav-card">
-                   <div className="col-md-12"> 
-                     <div className="row">
-                     <div className="col-md-2" id="nav-card">
-
-<img src={logo} alt="logo" />
-
-<button className="btn-hamburger openbtn" onClick={onToggle}>
-
-    <img src={hambuger} alt="logo" />
-
-</button>
-
-</div>
-                    <div className="col-md-2 pt-1" >
-                        <h5 className="nav-h4">Welcome, Admin</h5>
-                        <p>Quarter <span>:</span><strong><span>2023-24 Q1</span></strong></p>
-                    </div>
-                    <div className="col-md-2" >
-                        <div id="third-nav" className="pt-1">
-                            <div >
-                            <p className="nav-para" >Channel <span>:</span><strong>All</strong></p>   
-                            </div>
-                            <div  className="nav-div">
-                            <img src={Navi} alt="navi" /> <strong>Mumbai</strong>
-                            </div>
-                        </div>
-                    </div>
-                    
-                    <div className="col-md-3 " id="nav-fourth">
-                        <button className="quarter">Select Quarter <span><i class="bi-arrow-down-square"></i></span></button>
-
-                        <button type="button" class="btn btn-group" id="nav-sixth">
-                            <img src={bell} alt="bell" width="15%" height="15%" />
-                            {/* <span className="ribbon">9</span> */}
-                        </button>
-                    </div>
-                      <div className="col-md-3" id="nav-fifth">
-                        <ul className="list-unstyled m-0">
-                            <li  id="nav-admin" >
-                                <img src={Profile}  alt="..." width="25%" height="25%" className="mt-4"/>
-                                <div  className="m-2 w-100">
-                                    <h5 className="nav-heading" >Role : Admin</h5>
-                                    <p >21 June 2023, 16:40</p>
-                                </div>
-                            </li>
-                        </ul>
-                      <div className="col-md-2" >  
-                        <button className="text-center btn-logout" onClick={handleLogout}>
-                              <img src={logout} alt="logout" />     
-                        </button>
-                      <strong>Logout</strong>  
-                      </div>  
-                    </div>
-                    </div> 
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-white p-0" >
+        <div className="container-fluid " id="nav-card">
+          <div className="col-md-12">
+            <div className="row">
+              <div className="col-md-2" id="nav-card">
+                <img src={logo} alt="logo" />
+                <button className="btn-hamburger openbtn" onClick={onToggle}>
+                  <img src={hambuger} alt="logo" />
+                </button>
+              </div>
+              <div className="col-md-2 pt-1" >
+                <h5 className="nav-h4">Welcome, Admin</h5>
+                <p>Quarter <span>:</span><strong><span>2023-24 Q1</span></strong></p>
+              </div>
+              <div className="col-md-2" >
+                <div id="third-nav" className="pt-1">
+                  <div >
+                    <p className="nav-para" >Channel <span>:</span><strong>All</strong></p>
+                  </div>
+                  <div className="nav-div">
+                    <img src={Navi} alt="navi" /> <strong>Mumbai</strong>
                   </div>
                 </div>
-            </nav>
-   
-          
-         
-
-
-        </>
-    )
-
-
+              </div>
+              <div className="col-md-3 " id="nav-fourth">
+                <button className="quarter">Select Quarter <span><i class="bi-arrow-down-square"></i></span></button>
+                <button type="button" class="btn btn-group" id="nav-sixth">
+                  <img src={bell} alt="bell" width="15%" height="15%" />
+                </button>
+              </div>
+              <div className="col-md-3" id="nav-fifth">
+                <ul className="list-unstyled m-0">
+                  <li id="nav-admin" >
+                    <img src={Profile} alt="..." width="25%" height="25%" className="mt-4" />
+                    <div className="m-2 w-100">
+                      <h5 className="nav-heading" >Role : Admin</h5>
+                      <p >21 June 2023, 16:40</p>
+                    </div>
+                  </li>
+                </ul>
+                <div className="col-md-2" >
+                  <button className="text-center btn-logout" onClick={handleLogout}>
+                    <img src={logout} alt="logout" />
+                  </button>
+                  <strong>Logout</strong>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  )
 }
 export default Navbar;
+
