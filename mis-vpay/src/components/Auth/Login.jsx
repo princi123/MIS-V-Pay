08d98ca2159cf890742a01d7c3d0533d6893db2d
kunal413 <kunalsharmaraj09@gmail.com>
2023-09-05 10:59:51 +0530

    show password
    while clicking checkbox show the password

--- mis-vpay/src/components/Auth/Login.jsx
+++ mis-vpay/src/components/Auth/Login.jsx
@@ -2,62 +2,58 @@ import React, { useEffect, useState } from "react";
 import "../Assets/css/Auth/Login.css";
 import leftimage from "../Assets/images/utiloginfinal.png";
 import { useNavigate } from "react-router-dom";
-import {
-  setEmpIdCookie,
-  setAuthTokenCookie,
-} from "./Cookie";
+import { setEmpIdCookie, setAuthTokenCookie } from "./Cookie";
 import { API_LOGIN } from "../../Constant/apiConstant";
 import Api from "../Retail/RetailApi/Api";
 
-
 const Login = () => {
-    const [p_emp_id, setEmpID] = useState(" ");
-    const [password, setPassword] = useState("");
-    const navigate = useNavigate();
-  
-    const handleLogin = (e) => {
-      e.preventDefault();
-      fetch(API_LOGIN.DATA, {
-        method: "POST",
-        body: JSON.stringify({ p_emp_id, password }),
-        headers: {
-          "Content-Type": "application/json",
-        },
+  const [p_emp_id, setEmpID] = useState(" ");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
+  const handleLogin = (e) => {
+    e.preventDefault();
+    fetch(API_LOGIN.DATA, {
+      method: "POST",
+      body: JSON.stringify({ p_emp_id, password }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
       })
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error("Network response was not ok");
-          }
-          return response.json();
-        })
-        .then((data) => {
-          if (Array.isArray(data) && data.length > 0) {
-            const empId = data[0].p_emp_id;
-            const token = data[0].p_auth_token;
-  
-            setEmpIdCookie(empId);
-            setAuthTokenCookie(token);
-  
-            const headers = {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${token}`,
-              emp_id: empId,
-            };
-  
-            setEmpID("");
-            setPassword("");
-            navigate("/Home");
-            // Render the Api component and pass headers as a prop
-            console.log("kk");
-            return <Api headers={headers} />;
-          } else {
-            console.error("Invalid API response format");
-          }
-        })
-        .catch((error) => {
-          console.error("Error fetching data:", error);
-        });
-    };
+      .then((data) => {
+        if (Array.isArray(data) && data.length > 0) {
+          const empId = data[0].p_emp_id;
+          const token = data[0].p_auth_token;
+
+          setEmpIdCookie(empId);
+          setAuthTokenCookie(token);
+
+          const headers = {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+            emp_id: empId
+          };
+
+          setEmpID("");
+          setPassword("");
+          navigate("/Home");
+          // Render the Api component and pass headers as a prop
+          console.log("kk");
+          return <Api headers={headers} />;
+        } else {
+          console.error("Invalid API response format");
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  };
 
   return (
     <>
@@ -75,46 +71,51 @@ const Login = () => {
               <div className="main-form">
                 <form>
                   <div className="mb-3">
-                    <label className="form-label" id="label-text">
-                      Employee ID <span className="required-span">*</span>
-                    </label>
-                    <input
-                      type="text"
-                      className="form-control "
-                      placeholder="Please Enter your valid Employee ID"
-                      value={p_emp_id}
-                      onChange={(e) => {
+                      <label className="form-label" id="label-text">
+                         Employee ID <span className="required-span">*</span>
+                      </label>
+                      <input
+                        type="text"
+                        className="form-control "
+                        placeholder="Please Enter your valid Employee ID"
+                        value={p_emp_id}
+                        onChange={(e) => {
                         setEmpID(e.target.value);
-                      }}
-                    />
+                        }}
+                      />
                   </div>
                   <div className="mb-3">
-                    <label className="form-label" id="label-text">
-                      Password <span className="required-span">*</span>
-                    </label>
-                    <input
-                      type="password"
-                      className="form-control "
-                      id="exampleInputPassword1"
-                      placeholder="Please Enter your Password"
-                      value={password}
-                      onChange={(e) => {
-                        setPassword(e.target.value);
-                      }}
-                    />
+                        <label className="form-label" id="label-text">
+                           Password <span className="required-span">*</span>
+                       </label>
+                        <input
+                        type={showPassword ? "text" : "password"} // Toggle the input type based on showPassword state
+                        className="form-control"
+                        id="exampleInputPassword1"
+                        placeholder="Please Enter your Password"
+                        value={password}
+                        onChange={(e) => {
+                            setPassword(e.target.value);
+                        }}
+                        />
                   </div>
                   <div className="mb-3 form-check">
-                    <input type="checkbox" className="form-check-input" />
-                    <label className="form-check-label">Show Password</label>
+                        <input
+                        type="checkbox"
+                        className="form-check-input"
+                        checked={showPassword}
+                        onChange={() => setShowPassword(!showPassword)}
+                        />
+                        <label className="form-check-label">Show Password</label>
                   </div>
                   <div className="text-center">
-                    <button
-                      className="btn w-100"
-                      id="button-login"
-                      onClick={handleLogin}
-                    >
-                      Login
-                    </button>
+                        <button
+                        className="btn w-100"
+                        id="button-login"
+                        onClick={handleLogin}
+                        >
+                        Login
+                        </button>
                   </div>
                 </form>
               </div>
