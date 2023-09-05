54542c57db665f4bd03986865dacf7b0da23ea51
Uday-glict <84915848+Uday-glict@users.noreply.github.com>
2023-09-05 15:42:04 +0530

    AUM-API

--- mis-vpay/src/components/Retail/AUM/Search.jsx
+++ mis-vpay/src/components/Retail/AUM/Search.jsx
@@ -1,188 +1,26 @@
-// import { React, useEffect, useState } from "react";
-// import "../AUM/Search.css";
-// import Navbar from "../../Shared/Navbar";
-// import SideBar from "../../Shared/SideBar/SideBar";
-// import Aum from "./Aum";
-// import {API_AUM_Region } from '../../../Constant/apiConstant';
-// import Loading from "../Loading";
-// import { AUM_Api } from "../RetailApi/AUM_Api";
-
-// const Search = (props) => {
- 
-//   const [sidebarOpen, setSidebarOpen] = useState(false);
-//   // const [aum_details, setAumDetails] = useState([]);
-//   const [hide,setHide]=useState(false);
-//   const toggleSidebar = () => {
-//     setSidebarOpen(!sidebarOpen);
-//   };
-  
- 
- 
-//   // const fetchData = async () => {
-//   //   try {
-//   //     const response = await fetch(API_AUM_Region.DATA);
-//   //     const data = await response.json();
-//   //     setAumDetails(data);
-//   //     console.log(aum_details,"aum_details")
-//   //   } catch (error) {
-//   //     console.error("error fetching scheme details", error);
-//   //   }
-//   // };
-
- 
-//   const SearchOnClick=()=>{
-//      AUM_Api();
-//      setHide(true)
-
-//   }
-//   return (
-//     <div className="container-fluid p-0 home-main ">
-//       <Navbar onToggle={toggleSidebar} />
-//       <div className="d-flex">
-//         <SideBar isOpen={sidebarOpen} />
-//         <div className={` ${sidebarOpen ? "dashboard-closed" : "dashboard-full"}`}>
-//           <div className="bg-white card m-4" style={{ borderRadius: "10px" }}>
-//             <div className="col-md-12">
-//               <div className="headline pt-4" style={{ paddingLeft: "20px" }}>
-//                 <b>Daily Retail RH CH RM Zone Wise Performance</b>
-//               </div>
-//               <div className="d-flex justify-content-center mb-5 mt-5">
-//                 <div className="col-md-8 d-flex">
-//                   <div className="col-md-6 ">
-//                     <label style={{ paddingLeft: "10px" }}>
-//                       <b>Select Period</b>
-//                     </label>
-//                     <select className="form-select m-2">
-//                       <option value="">Select an option</option>
-//                       <option value="option1">Option 1</option>
-//                       <option value="option2">Option 2</option>
-//                       <option value="option3">Option 3</option>
-//                     </select>
-//                   </div>
-//                   <div
-//                     style={{
-//                       marginLeft: "10px",
-//                       marginTop: "19px",
-//                       padding: "10px 10px 10px 10px",
-//                     }}
-//                   >
-//                     <button className="BgcolorOrange btn" onClick={SearchOnClick} style={{ marginRight: "5px", padding: "11px" }}>
-//                       Search
-//                     </button>
-//                     <button className="BgcolorOrange btn " style={{ marginRight: "5px", padding: "11px" }} >
-//                       Export
-//                     </button>
-//                   </div>
-//                 </div>
-//               </div>
-//             </div>
-//            {hide ?
-//             <Aum />
-//             :""
-//            }
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Search;
-
-// import React, { useEffect, useState } from "react";
-// import "../AUM/Search.css";
-// import Navbar from "../../Shared/Navbar";
-// import SideBar from "../../Shared/SideBar/SideBar";
-// import Aum from "./Aum";
-// import { API_AUM_Region } from '../../../Constant/apiConstant';
-// import Loading from "../Loading";
-// // Import your AUM_Api function correctly
-// import { AUM_Api } from "../RetailApi/AUM_Api";
-
-// const Search = (props) => {
-//   const [sidebarOpen, setSidebarOpen] = useState(false);
-//   const [hide, setHide] = useState(false);
-//   const [aumDetails, setAumDetails] = useState([]);
-//   const toggleSidebar = () => {
-//     setSidebarOpen(!sidebarOpen);
-//   };
-
-//   const SearchOnClick = async () => {
-//     const aumData = AUM_Api();
-//     console.log("bbbb")
-//     setAumDetails(aumData);
-//     setHide(true);
-//   };
-
-//   return (
-//     <div className="container-fluid p-0 home-main">
-//       <Navbar onToggle={toggleSidebar} />
-//       <div className="d-flex">
-//         <SideBar isOpen={sidebarOpen} />
-//         <div className={` ${sidebarOpen ? "dashboard-closed" : "dashboard-full"}`}>
-//           <div className="bg-white card m-4" style={{ borderRadius: "10px" }}>
-//             <div className="col-md-12">
-//               <div className="headline pt-4" style={{ paddingLeft: "20px" }}>
-//                 <b>Daily Retail RH CH RM Zone Wise Performance</b>
-//               </div>
-//               <div className="d-flex justify-content-center mb-5 mt-5">
-//                 <div className="col-md-8 d-flex">
-//                   <div className="col-md-6">
-//                     <label style={{ paddingLeft: "10px" }}>
-//                       <b>Select Period</b>
-//                     </label>
-//                     <select className="form-select m-2">
-//                       <option value="">Select an option</option>
-//                       <option value="option1">Option 1</option>
-//                       <option value="option2">Option 2</option>
-//                       <option value="option3">Option 3</option>
-//                     </select>
-//                   </div>
-//                   <div
-//                     style={{
-//                       marginLeft: "10px",
-//                       marginTop: "19px",
-//                       padding: "10px 10px 10px 10px",
-//                     }}
-//                   >
-//                     <button className="BgcolorOrange btn" onClick={SearchOnClick} style={{ marginRight: "5px", padding: "11px" }}>
-//                       Search
-//                     </button>
-//                     <button className="BgcolorOrange btn " style={{ marginRight: "5px", padding: "11px" }}>
-//                       Export
-//                     </button>
-//                   </div>
-//                 </div>
-//               </div>
-//             </div>
-//             {hide ? <Aum aumDetails={aumDetails}  /> : ""}
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Search;
-
 import React, { useState } from "react";
 import "../AUM/Search.css";
 import Navbar from "../../Shared/Navbar";
 import SideBar from "../../Shared/SideBar/SideBar";
 import Aum from "./Aum";
 import { useAUMApi } from "../RetailApi/AUM_Api";
+import { usePeriodApi } from "../RetailApi/AUM_Api";
+
 
 const Search = (props) => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [hide, setHide] = useState(false);
-  const aumDetails = useAUMApi(); // Call useAUMApi hook to get data
+  const aumDetails = useAUMApi();
+  const aumPeriod = usePeriodApi();
 
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
   };
 
-  const SearchOnClick = () => {
+  const SearchOnClick = (e) => {
     console.log(aumDetails)
+    console.log(aumPeriod)
+   
     setHide(true);
   };
 
@@ -217,7 +55,11 @@ const Search = (props) => {
                       padding: "10px 10px 10px 10px",
                     }}
                   >
-                    <button className="BgcolorOrange btn" onClick={SearchOnClick} style={{ marginRight: "5px", padding: "11px" }}>
+                    <button
+                      className="BgcolorOrange btn"
+                      onClick={(e)=>SearchOnClick(e)}
+                      style={{ marginRight: "5px", padding: "11px" }}
+                    >
                       Search
                     </button>
                     <button className="BgcolorOrange btn " style={{ marginRight: "5px", padding: "11px" }}>
@@ -227,7 +69,7 @@ const Search = (props) => {
                 </div>
               </div>
             </div>
-            {hide ? <Aum  /> : ""}
+            {hide ? <Aum /> : ""}
           </div>
         </div>
       </div>
