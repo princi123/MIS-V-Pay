45001891aa6d6d9abe4a70248cd6a11c3fe814e1
Kirtitandulkar362000 <115093565+Kirtitandulkar362000@users.noreply.github.com>
2023-09-13 14:29:41 +0530

    complete correction

--- app/controllers/api/v1/aum_regions_controller.rb
+++ app/controllers/api/v1/aum_regions_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::AumRegionsController < ApplicationController
 	def index
 		# report_period = params[:report_period]
-    # zone = params[:zone]
+		# zone = params[:zone]
 		aum_region_details = AumRegionService.get_aum_region_details
 		render json: aum_region_details    
   end   
