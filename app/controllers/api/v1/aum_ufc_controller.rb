6ac6089050a245833f624c85b32cd0e6f9451b19
Kirtitandulkar362000 <115093565+Kirtitandulkar362000@users.noreply.github.com>
2023-09-15 17:26:12 +0530

    AUM sp changes, api integration , code correction complete

--- app/controllers/api/v1/aum_ufc_controller.rb
+++ app/controllers/api/v1/aum_ufc_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::AumUfcController < ApplicationController
   def index
-		aum_ufc_details = AumUfcService.get_aum_ufc_details
+    report_period = params[:report_period]
+		zone = params[:zone]
+    region_code = params[:region_code]
+		aum_ufc_details = AumUfcService.get_aum_ufc_details(report_period, zone, region_code)
 		render json: aum_ufc_details    
   end   
 end
--- app/services/aum_ufc_service.rb
+++ app/services/aum_ufc_service.rb
@@ -1,9 +1,11 @@
 class AumUfcService
-    def self.get_aum_ufc_details
+    def self.get_aum_ufc_details(report_period, zone, region_code)
       conn = OCI8.new('MISVPAY', 'MISVPAY@123', '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=103.12.1.155)(PORT=1521))(CONNECT_DATA=(SID=xe)))')
       procedure_name = 'sp_misvpay_get_aum_based_reports_ufc_wise'
-      cursor = conn.parse("BEGIN #{procedure_name}(:get_all_data); END;")
-  
+      cursor = conn.parse("BEGIN #{procedure_name}(:p_report_period,:p_zone,:p_region_code,:get_all_data); END;")
+      cursor.bind_param(':p_report_period', report_period, String)
+      cursor.bind_param(':p_zone', zone, String)
+      cursor.bind_param(':p_region_code', region_code, String)
       cursor.bind_param(':get_all_data', nil, OCI8::Cursor)    
       cursor.exec
       ufc_report = []
